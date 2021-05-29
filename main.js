281a03c7aa68ec54553bacf0e7f1c4c06cc37fdf
hortenssiaa <hortenssiaa@gmail.com>
2021-05-29 19:50:48 +0900

    JS : Work filtering

--- main.js
+++ main.js
@@ -59,6 +59,47 @@ arrowUp.addEventListener('click', () => {
 
 
 
+// Projects
+// check more at Hortenssiaa's blog
+const workBtnContainer = document.querySelector(".work__categories");
+const projectsContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+const categoryBtnContainerArr = document.querySelectorAll(".category__btn");
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+
+    // Change clicked category button color  
+    categoryBtnContainerArr.forEach((categoryBtn) => {
+        if (categoryBtn.dataset.filter === filter) {
+            categoryBtn.classList.add('active');
+        } else {
+            categoryBtn.classList.remove('active');
+        }
+    });
+
+    // 다 보여지고난 다음에 (giving some times for anim-out)
+    projectsContainer.classList.add("anim-out");
+    
+    // after 0.3s, filtering with IF-ELSE then remove anim-out (-> make it's opacity : 1)
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if (filter === "*" || project.dataset.type === filter) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+
+        projectsContainer.classList.remove("anim-out");
+    }, 270);
+
+});
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior : 'smooth'});
